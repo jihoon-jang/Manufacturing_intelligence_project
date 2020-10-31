84ca2729982cce4638c6a9ce372f2e66dbde8148
ji_hoon <jigns153@naver.com>
2020-10-31 21:13:14 +0900

    query filter 2개 이상

--- api/schema.py
+++ api/schema.py
@@ -10,11 +10,11 @@ class invenType(DjangoObjectType):
 
 class invenQuery(graphene.ObjectType):
     all_inven = graphene.List(invenType)
-    date_inven = graphene.Field(all_inven, bsnscd=graphene.String())
+    date_inven = graphene.Field(all_inven, iDate=graphene.String(), bsnscd=graphene.String())
 
 
     def resolve_all_inven(self, info, **kwargs):
         return inventory.objects.all()
 
-    def resolve_date_inven(self, info, bsnscd):
-        return inventory.objects.filter(bsnscd=bsnscd)
+    def resolve_date_inven(self, info, iDate, bsnscd):
+        return inventory.objects.filter(i_date=iDate, bsnscd=bsnscd)
